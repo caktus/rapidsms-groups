9eb859ab0c1f69e092162580c646ed57a30903fa
Tobias McNulty <tobias@caktusgroup.com>
2014-05-23 11:25:32 -0400

    move rapidsms model extension to its own model with a one-to-one field to Contact

--- groups/models.py
+++ groups/models.py
@@ -1,8 +1,28 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from django.db import models
 
 from rapidsms.models import Contact
+
+from groups.utils import format_number
+
+
+class GroupContact(models.Model):
+    """ Model to extend the RapidSMS Contact model """
+    contact = models.OneToOneField(Contact)
+    first_name = models.CharField(max_length=64, blank=True)
+    last_name = models.CharField(max_length=64, blank=True)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=32, blank=True)
+    title = models.CharField(max_length=64, blank=True)
+
+    def save(self, **kwargs):
+        self.name = '%s %s' % (self.first_name, self.last_name)
+        super(GroupContact, self).save(**kwargs)
+
+    @property
+    def formatted_phone(self):
+        return format_number(self.phone)
 
 
 class Group(models.Model):
@@ -11,7 +31,7 @@ class Group(models.Model):
     description = models.TextField(blank=True)
     is_editable = models.BooleanField(default=True)
 
-    contacts = models.ManyToManyField(Contact, related_name='groups',
+    contacts = models.ManyToManyField(GroupContact, related_name='groups',
                                       blank=True)
 
     def __unicode__(self):
